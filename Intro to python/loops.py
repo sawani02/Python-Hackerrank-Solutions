0655b089671631234f89748dfdbf393a41428272
Sawani Soman <sawani.soman92@gmail.com>
2018-06-18 16:01:42 -0700

    Create loops.py

---
+++ Intro to python/loops.py
@@ -0,0 +1,10 @@
+#Cahllenge link: https://www.hackerrank.com/challenges/python-loops/submissions/code/54269084
+if __name__ == '__main__':
+    n = int(input())
+    
+   
+    for i in range(0, n):
+    
+        i = i ** 2
+    
+        print(i)
